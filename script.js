0532b25bb505b0be8523d8fc9dbfae61cf68a98e
WILIBERTUSJARUT1992 <wilibertusjarut124@gmail.com>
2026-07-05 22:04:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
 
-    if (username === "admin" && password === "manggarai123") {
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Username atau Password salah!");
-    }
+  if (user === "admin" && pass === "1234") {
+    localStorage.setItem("login", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Login gagal!");
+  }
+}
+
+// cek akses dashboard
+function cekLogin() {
+  if (localStorage.getItem("login") !== "true") {
+    window.location.href = "index.html";
+  }
+}
+
+// logout
+function logout() {
+  localStorage.removeItem("login");
+  window.location.href = "index.html";
 }
